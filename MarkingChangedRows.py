d515eb4b168ee05f9d5dbc6eb0d0422f6a2e24cd
Harurow <Harurow@users.noreply.github.com>
2013-07-19 15:14:17 +0900

    remove .sublime-settings

--- MarkingChangedRows.py
+++ MarkingChangedRows.py
@@ -6,23 +6,15 @@ _MARK_STYLE = sublime.HIDE_ON_MINIMAP | sublime.PERSISTENT | sublime.HIDDEN
 
 class MarkingChangedRowsCommand(sublime_plugin.EventListener):
 	def on_modified(self, view):
-		changed = view.get_regions('changed')
-		changed +=[view.line(r) for r in view.sel()]
-		view.add_regions('changed', changed, 'mark', 'dot', _MARK_STYLE)
+		if view.file_name():
+			changed = view.get_regions('changed')
+			changed +=[view.line(r) for r in view.sel() if not r in changed]
+
+			view.add_regions('changed', changed, 'mark', 'dot', _MARK_STYLE)
+			if view.is_dirty():
+				view.add_regions('changed', changed, 'mark', 'dot', _MARK_STYLE)
+			else:
+				view.erase_regions('changed')
 
 	def on_post_save(self, view):
-		settings = sublime.load_settings('MarkingChangedRows.sublime-settings')
-
-		if not settings.get('clear_on_saved'):
-			tmp = view.get_regions('changed')
-			tmp += view.get_regions('saved')
-
 			view.erase_regions('changed')
-			view.erase_regions('saved')
-
-			if not settings.get('clear_on_saved'):
-				view.add_regions('saved', tmp, 'comment', 'dot', _MARK_STYLE)
-		else:
-			view.erase_regions('changed')
-			view.erase_regions('saved')
-
