fe2e4258fc8d738e3a131de988375b97649c932f
Harurow <Harurow@users.noreply.github.com>
2013-10-30 11:43:20 +0900

    fix
    複数行の修正時場合、各行にマークがついていなかった。

--- MarkingChangedRows.py
+++ MarkingChangedRows.py
@@ -1,23 +1,33 @@
 # -*- coding: utf-8 -*-
+
 import sublime
 import sublime_plugin
 
+settings = sublime.load_settings("MarkingChangedRows.sublime-settings")
+_KEY = 'marking_changed'
 _MARK_STYLE = sublime.HIDE_ON_MINIMAP | sublime.PERSISTENT | sublime.HIDDEN
+_ICON = settings.get("icon")
+_SCOPE = settings.get("scope")
 
 class MarkingChangedRowsCommand(sublime_plugin.EventListener):
 	def on_modified(self, view):
-		if view.file_name():
-			changed = view.get_regions('changed')
-			changed +=[view.line(r) for r in view.sel() if not r in changed]
+		if not view.file_name():
+			return
 
-			view.add_regions('changed', changed, 'mark', 'dot', _MARK_STYLE)
-			if view.is_dirty():
-				view.add_regions('changed', changed, 'mark', 'dot', _MARK_STYLE)
-			else:
-				view.erase_regions('changed')
+		if view.is_dirty():
+			changed = view.get_regions(_KEY)
+			view.erase_regions(_KEY);
+
+			for sel in view.sel():
+				for r in view.lines(sel):
+					if not r in changed: 
+						changed.append(r)
+			view.add_regions(_KEY, changed, _SCOPE, _ICON, _MARK_STYLE)
+		else:
+			view.erase_regions(_KEY);
 
 	def on_post_save(self, view):
-			view.erase_regions('changed')
+		view.erase_regions(_KEY)
 
 	def on_load(self, view):
-			view.erase_regions('changed')
+		view.erase_regions(_KEY)
